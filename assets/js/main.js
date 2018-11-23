7ec02a74245af90b3bfa2aa072b32be9f011a693
Daniel Andrade <danielsilva97.andrade@gmail.com>
2018-11-23 17:07:36 -0200

    css com botao de reset adicionado, modais e avisos no html ao trocar de papel, troca de papel e cores funcionando com js #commitFinal

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,33 @@ var brush = canvas.getContext('2d');
 
 var canvasPosition = canvas.getBoundingClientRect();
 
-// Cores experimentais //
+// COR PADRAO PARA O PAPEL //
 var paperColor = '#ffff99'
 brush.fillStyle = paperColor;
 brush.fillRect(0, 0, 600, 400);
 
-// function canvasColor() { color = document.querySelector('canvas').style.backgroundColor; return color; }
+// TROCA A COR DO NOVO PAPEL //
+var shapeBackgroundColor = document.querySelector('.background-color');
+shapeBackgroundColor.addEventListener('click', newPaperColor);
+
+function newPaperColor(event){
+  clickedObject = event.target;
+  var style = getComputedStyle(clickedObject);
+  var currentColor = style.backgroundColor;
+  paperColor = currentColor;
+};
+
+
+// TROCA DE PAPEL //
+var newArt = document.querySelector('#newArt');
+newArt.addEventListener('click', changePaper);
+
+function changePaper(event){
+  brush.fillStyle = paperColor;
+  brush.fillRect(0, 0, 600, 400);
+}
+
+
+// BOTAO DE RESET DA ARTE //
+var resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', changePaper);
